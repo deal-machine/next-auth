b2ffd1dded3386ab942cd7af00083c14cb2ca481
deal-machine <95deal@gmail.com>
2022-06-01 19:33:53 -0300

    feat: create state in inputs

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,20 +1,45 @@
+import { useState } from "react";
+
 export default function HomeScreen() {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+
+    setValues((currentValues) => ({
+      ...currentValues,
+      [fieldName]: fieldValue,
+    }));
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
         <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          placeholder="Usuário"
+          name="username"
+          value={values.username}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          placeholder="Senha"
+          name="password"
+          type="password"
+          value={values.password}
+          onChange={handleChange}
         />
+        {console.log(values)}
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
     </div>
